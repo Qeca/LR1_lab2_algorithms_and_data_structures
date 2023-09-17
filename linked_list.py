a180f5f7dab06c642ebee95191d23723fe2feeea
4219 Sharonov D.V <dima.sharonov.2004@mail.ru>
2023-09-17 18:06:45 +0300

    Дополнил односвязанный список методом удаления элемента по индексу, вывод длинны списка, получения элемента по индексу. Создал файл для написания юнит тестов для односвязного списка.

--- linked_list.py
+++ linked_list.py
@@ -1,24 +1,31 @@
 class LinkedList:
 
-    class Node:
+    class __Node:
 
-        def __init__(self, element=None, next_node=None):
+        def __init__(self, element=None, next_node=None) -> None:
             self.element = element
             self.next_node = next_node
 
-    def __init__(self, __head = None):
-        self.__head = __head
 
-    def append(self, element):
+    def __init__(self, head=None, length=0) -> None:
+        if head != None and length != 0:
+            raise ValueError("В инициализатор не суй ничего")
+        self.__head = head
+        self.__length = length
+
+
+
+    def append(self, element) -> None:
         if not self.__head:
-            self.__head = self.Node(element)
+            self.__head = self.__Node(element)
             return element
         node = self.__head
 
         while node.next_node:
             node = node.next_node
 
-        node.next_node = self.Node(element)
+        node.next_node = self.__Node(element)
+        self.__length += 1
 
     @property
     def out(self) -> str:
@@ -30,12 +37,63 @@ class LinkedList:
         llist_string += f'{node.element}]'
         return llist_string
 
+    def insert(self,element,index: int):
+        if index < 0:
+            raise IndexError("Вставка по отрицательному индексу не предусмотрена")
+        elif index > self.__length:
+            raise IndexError("Выход за границы списка")
+        i = 0
+        node = self.__head
+        while i < index:
+            prev_node = node
+            node = node.next_node
+            i += 1
+        prev_node.next_node = self.__Node(element, next_node=node)
+        self.__length += 1
+        return element
+
+    def get_elem(self,index):
+        if index < 0:
+            raise IndexError("Получение элемента по отрицательному индексу не предусмотрено")
+        elif index > self.__length:
+            raise IndexError("Выход за границы списка")
+        i = 0
+        node = self.__head
+        prev_node = self.__head
+        while i < index:
+            prev_node = node
+            node = node.next_node
+            i += 1
+
+        return node.element
+
+    def size(self) -> int:
+        return self.__length + 1
+
+    def delete(self, index):
+        if index < 0:
+            raise IndexError("Удаление по отрицательному индексу не предусмотрено")
+        elif index > self.__length:
+            raise IndexError("Выход за границы списка")
+        if index == 0:
+            self.__head = self.__head.next_node
+
+        node = self.__head
+        i = 0
+        prev_node = node
+        while i < index:
+            prev_node = node
+            node = node.next_node
+            i += 1
+
+        prev_node.next_node = node.next_node
+        element = node.element
+
+        del node
+
+        self.__length -= 1
+        return element
+
 
 llist = LinkedList()
 
-llist.append(1)
-llist.append(2)
-llist.append(3)
-llist.append(4)
-llist.append(5)
-print(llist.out)
